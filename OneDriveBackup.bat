b7a1319a9f972bf13295a91555e674c2c7d347bb
Alex Peterson <AlexJoseph.Peterson@CalBaptist.edu>
2021-03-26 16:15:02 -0700

    Initialized

---
+++ OneDriveBackup.bat
@@ -0,0 +1,30 @@
+echo off
+setlocal enabledelayedexpansion
+echo Syncing Lastest Version From OneDrive
+%LOCALAPPDATA%\Microsoft\OneDrive\OneDrive.exe /shutdown
+start %LOCALAPPDATA%\Microsoft\OneDrive\OneDrive.exe
+pause
+
+
+set /a argCount=1
+for %%x in ( %* ) do (
+    IF !argCount! LEQ 1 ( 
+        set destinationFolder="%%x"
+        cd /d !destinationFolder!
+        git pull
+    ) 
+    ELSE (
+        echo Copying    %%x      to the GitHub Directory !destinationFolder!
+        xcopy /s "%OneDrive%\%%x" /y /z
+
+        git add %%x
+    )
+    set /a argCount+=1
+)
+
+git commit -m"Auto Backup"
+git push
+
+echo Done
+pause
+endlocal
